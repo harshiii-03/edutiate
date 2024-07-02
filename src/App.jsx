0f31aaaf1f0aad90d4c3caf9629d8abf95a4e0c2
harshiii-03 <ninamaharshita0803@gmail.com>
2024-07-02 14:55:40 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import Navbar from './Components/Navbar/Navbar'
+import Hero from './Components/Hero/Hero'
+import Program from './Components/Programs/Program'
+import Title from './Components/Title/Title'
+import About from './Components/About/About'
+import Campus from './Components/Campus/Campus'
+import Testimonials from './Components/Testimonials/Testimonials'
+import Contact from './Components/Contact/Contact'
+import Footer from './Components/Footer/Footer'
+import VideoPlayer from './Components/VideoPlayer/VideoPlayer'
+
+const App = () => {
+
+  const [playState, setPlayState]=useState(false);
+
+
+
+  return (
+    <div>
+      <Navbar/>
+      <Hero/>
+      <div className="container">
+        <Title subTitle='OUR PROGRAM' title='What We Offer'/>
+        {/* title and subtitle are passed as props  */}
+         
+      <Program/>
+      <About setPlayState={setPlayState}/>
+      <Title subTitle='Gallery' title='Campus Photos'/>
+      <Campus/>
+      <Title subTitle='Testimonials' title='What the students says'/>
+      <Testimonials/>
+      <Title subTitle='Contact Us' title='Get In Touch'/>
+      <Contact/>
+      <Footer/>
+
+
+
+      </div>
+      <VideoPlayer playState={playState} setPlayState={setPlayState}/>
+    </div>
+  )
+}
+
+export default App
